70352a0ebf1b23931c862bacffb4a36469444c67
Stefan Bodewig <stefan.bodewig@aixigo.de>
2014-10-14 10:29:50 +0200

    MessageBodyReader Provider for HAL media types

---
+++ src/main/java/com/theoryinpractise/halbuilder/jaxrs/JaxRsHalBuilderReaderSupport.java
@@ -0,0 +1,34 @@
+package com.theoryinpractise.halbuilder.jaxrs;
+
+import com.theoryinpractise.halbuilder.api.ContentRepresentation;
+import com.theoryinpractise.halbuilder.api.RepresentationFactory;
+import com.theoryinpractise.halbuilder.standard.StandardRepresentationFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@Consumes({RepresentationFactory.HAL_JSON, RepresentationFactory.HAL_XML})
+public class JaxRsHalBuilderReaderSupport implements MessageBodyReader<ContentRepresentation> {
+
+    private final RepresentationFactory factory = new StandardRepresentationFactory();
+
+    @Override
+    public boolean isReadable(Class aClass, Type type, Annotation[] annotations, MediaType mediaType) {
+        return ContentRepresentation.class.isAssignableFrom(aClass) && HalBuilderMediaTypes.isSupported(mediaType);
+    }
+
+    @Override
+    public ContentRepresentation readFrom(Class aClass, Type type, Annotation[] annotations, MediaType mediaType, MultivaluedMap multivaluedMap, InputStream inputStream) throws IOException {
+	return factory.readRepresentation(mediaType.toString(), new InputStreamReader(inputStream, "UTF-8"));
+    }
+
+}
